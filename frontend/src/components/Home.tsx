cadea2e8b8011dadd1e2c68e46da08ffd8a84945
Joel Bremner <jbr257@uclive.ac.nz>
2024-04-24 10:51:50 +1200

    Attempting to upload photos, not working to well

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,7 @@
 const Home = () => {
+
     return (<h1>Welcome To Petitions</h1>)
+
 }
 
 export default Home;
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,54 +1,22 @@
-import { Alert, AlertTitle, Autocomplete, Avatar, Box, Button, Chip, Container, CssBaseline, FormControl, Grid, InputLabel, Link, MenuItem, Modal, OutlinedInput, Select, SelectChangeEvent, Snackbar, TextField, Theme, ThemeProvider, Typography, createTheme, useTheme} from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Alert, Avatar, Box, Button, Container, CssBaseline, Grid, Link, Snackbar, TextField, ThemeProvider, Typography, createTheme} from "@mui/material";
 import axios from "axios";
-import { Dayjs } from "dayjs";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import UploadIcon from '@mui/icons-material/Upload';
 import { Navigate } from "react-router-dom";
-
-const POS_TAG_HEIGHT = 48;
-const POS_TAG_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: POS_TAG_HEIGHT * 4.5 + POS_TAG_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-    return {
-      fontWeight:
-        personName.indexOf(name) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-}
+import Cookies from 'js-cookie';
 
 const Register = () => {
-    const theme = useTheme();
-    const [errorFlag, setErrorFlag] = React.useState(false);
-    const [errorMessage, setErrorMessage] = React.useState("");
     const [updateFlag, setUpdateFlag] = React.useState(true);
     const [fname, setFname] = React.useState("");
     const [lname, setLname] = React.useState("");
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
-    const [dob, setDOB] = React.useState<Dayjs | null>();
-    const [gender, setGender] = React.useState<string|null>();
-    const [height, setHeight] = React.useState<number|null>();
-    const [weight, setWeight] = React.useState<number|null>();
-    const [positions, setPositions] = React.useState<string[]>([]);
-    const [role, setRole] = React.useState("");
-    const [club, setClub] = React.useState<string | null>("");
-    const [openModal, setOpenModal] = React.useState(false);
     const [snackMessage, setSnackMessage] = React.useState("")
     const [snackOpenSuccess, setSnackOpenSuccess] = React.useState(false)
     const [snackOpenFail, setSnackOpenFail] = React.useState(false)
-    const [allClubs, setAllClubs] = React.useState<string[]>([]);
     const [response, setResponse] = React.useState(false);
+    const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
 
     const handleSnackCloseSuccess = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
@@ -64,14 +32,14 @@ const Register = () => {
         setSnackOpenFail(false);
     };
 
-    const handleOpenModal = () => setOpenModal(true);
-    const handleCloseModal = () => setOpenModal(false);
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+          setSelectedFile(event.target.files[0]);
+        }
+    };
 
     React.useEffect(() => {
-        const getUsers = () => {
-            
-        }
-        getUsers()
+
     }, [updateFlag])
 
     const parseUser = () => {
@@ -82,17 +50,43 @@ const Register = () => {
         } else {
             axios.post('http://localhost:4941/api/v1/users/register', { "firstName": fname, "lastName": lname, "email": email, "password": password})
                 .then(() => {
-                    setUpdateFlag(true);
-                    setFname("");
-                    setLname("");
-                    setEmail("");
-                    setPassword("");
-
-                    
                     setSnackOpenFail(false)
-                    setSnackMessage("User added successfully")
-                    setSnackOpenSuccess(true)
-                    setResponse(true)
+                    setSnackOpenSuccess(false)
+
+                    axios.post('http://localhost:4941/api/v1/users/login', { "email": email, "password": password})
+                    .then((response) => {
+                        setUpdateFlag(true);
+                        setFname("");
+                        setLname("");
+                        setEmail("");
+                        setPassword("");
+                        
+                        setSnackOpenFail(false)
+                        setSnackOpenSuccess(false)
+                        Cookies.set('X-Authorization', response.data.token);
+                        Cookies.set('userId', response.data.userId);
+                        setResponse(true)
+
+                        if (selectedFile !== null) {
+                            console.log("Photo found");
+                            axios.put(`http://localhost:4941/api/v1/users/${Cookies.get('userId')}/image`, selectedFile)
+                                .then((res) => {
+                                    console.log("Request sent");
+                                    console.log(res)
+                                    setSelectedFile(null);
+                                    setSnackOpenFail(false)
+                                    setSnackOpenSuccess(false)
+                                }, (error) => {
+                                    setSnackMessage(error.response.statusText)
+                                    setSnackOpenFail(true)
+                                })
+                        }
+
+                    }, (error) => {
+                        setSnackMessage(error.response.statusText)
+                        setSnackOpenFail(true)
+                    })
+
                 }, (error) => {
                     setSnackMessage(error.response.statusText)
                     setSnackOpenFail(true)
@@ -100,26 +94,19 @@ const Register = () => {
         }
     }
 
-    // const input = {
-    //     margin: '5px',
-    //     padding: '5px'
-    // }
+    const checkValidSession = () => {
+        if (Cookies.get('sessionId')) {
+          return true;
+        }
+  
+        return false;
+    }
 
     const defaultTheme = createTheme();
     if (response) {
-        return (<Navigate to = {{ pathname: "/users/login" }} />)
-    } else if (errorFlag) {
-        return (
-            <div>
-                <h1>Users</h1>
-                {errorFlag &&
-                    <Alert severity="error">
-                        <AlertTitle>Error</AlertTitle>
-                        {errorMessage}
-                    </Alert>
-                }
-            </div>
-        )
+        return (<Navigate to = {{ pathname: "/home" }} />)
+    } else if (checkValidSession()) {
+        return (<Navigate to = {{ pathname: "/home" }} />)
     } else {
         return (
             <div>
@@ -142,72 +129,79 @@ const Register = () => {
                         </Typography>
                         <Box sx={{ mt: 3 }}>
                             <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                autoComplete="given-name"
-                                name="firstName"
-                                required
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                    value={fname} 
+                                    onChange={(event) => setFname(event.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                    value={lname} 
+                                    onChange={(event) => setLname(event.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    value={email} 
+                                    onChange={(event) => setEmail(event.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    value={password}
+                                    onChange={(event) => setPassword(event.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                        <TextField
+                                        fullWidth
+                                        color="secondary"
+                                        type="file" 
+                                        onChange={handleFileChange}/>
+                                </Grid>
+                            </Grid>
+                                <Button
+                                type="submit"
                                 fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                                value={fname} 
-                                onChange={(event) => setFname(event.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="family-name"
-                                value={lname} 
-                                onChange={(event) => setLname(event.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                value={email} 
-                                onChange={(event) => setEmail(event.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
-                                />
-                            </Grid>
-                            </Grid>
-                            <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={() => { parseUser() }}
-                            >
-                            Sign Up
-                            </Button>
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={() => { parseUser() }}
+                                >
+                                    Sign Up
+                                </Button>
                             <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="/users/login" variant="body2">
-                                Already have an account? Sign in
-                                </Link>
-                            </Grid>
+                                <Grid item>
+                                    <Link href="/users/login" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
                             </Grid>
                         </Box>
                         </Box>
