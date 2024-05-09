28b4ad41c1b5abd5c0b0f84fa16a74f92260190f
Joel Bremner <jbr257@uclive.ac.nz>
2024-05-10 01:14:43 +1200

    Can now filter by categories

--- frontend/src/components/Petitions.tsx
+++ frontend/src/components/Petitions.tsx
@@ -1,15 +1,47 @@
-import { Dialog, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Box, Chip, Dialog, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Paper, Select, SelectChangeEvent, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Theme } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import NavBar from './NavBar';
 import CSS from 'csstype';
 import {useSearchStore} from "../store";
 
+const POS_TAG_HEIGHT = 48;
+const POS_TAG_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: POS_TAG_HEIGHT * 4.5 + POS_TAG_PADDING_TOP,
+        width: 250,
+      },
+    },
+};
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+    return {
+      fontWeight:
+        personName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+}
+
+
 const Petitions = () => {
     const search = useSearchStore(state => state.search)
     const [petitions, setPetitions] = React.useState<petitionReturn>({petitions: [], count: 0});
     const [categories, setCategories] = React.useState<category[]>([]);
     const [supportTiers, setSupportTier] = React.useState<petitionSupportTiers[]>([]);
+    const [filterCats, setFilterCats] = React.useState<String[]>([]);
+
+    const updateFilterCats = (event: SelectChangeEvent<typeof filterCats>) => {
+        const {
+          target: { value },
+        } = event;
+        setFilterCats(
+          // On autofill we get a stringified value.
+          typeof value === 'string' ? value.split(',') : value,
+        );
+      };
 
     interface HeadCell {
         id: string;
@@ -34,17 +66,34 @@ const Petitions = () => {
     React.useEffect(() => {
         getAllPetitions()
         getCategories()
-    }, [search])
+    }, [search, filterCats])
+
+    const parseCategories = () => {
+
+        let resultString = ""
+        filterCats.map((cat: String) =>
+            {
+                console.log(cat)
+                const categoryId = categories.find(category => category.name === cat);
+                resultString = resultString + "&categoryIds=" +categoryId?.categoryId + ","
+            }
+        ) 
+
+        console.log(resultString.slice(0, -1))
+        return (resultString.slice(0, -1))
+    }
 
     const getAllPetitions = () => {
+        const parsedCatergories = parseCategories()
+
         if (search === "") {
-            axios.get("http://localhost:4941/api/v1/petitions?count=10")
+            axios.get("http://localhost:4941/api/v1/petitions?count=10" + parsedCatergories)
             .then((reponse) => {
                 setPetitions(reponse.data)
                 getSupportTiers(reponse.data)
             })
         } else {
-            axios.get("http://localhost:4941/api/v1/petitions?count=10&q=" + search)
+            axios.get("http://localhost:4941/api/v1/petitions?count=10&q=" + search + parsedCatergories)
             .then((reponse) => {
                 setPetitions(reponse.data)
                 getSupportTiers(reponse.data)
@@ -61,7 +110,6 @@ const Petitions = () => {
     }
 
     const getSupportTiers = (petitions: petitionReturn) => {
-        console.log("getSupportTiers")
         let minSupportTiers: petitionSupportTiers[] = [];
         petitions.petitions.map(async (row: petition) => 
             {
@@ -100,9 +148,50 @@ const Petitions = () => {
         )
     }
 
+    const FilterCategories = () => {
+        return (
+            <FormControl fullWidth>
+                <InputLabel id="demo-multiple-chip-label">Categories</InputLabel>
+                <Select
+                labelId="demo-multiple-chip-label"
+                id="demo-multiple-chip"
+                multiple
+                value={filterCats}
+                onChange={updateFilterCats}
+                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                renderValue={(selected) => (
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                    {selected.map((value) => (
+                        <Chip label={value} />
+                    ))}
+                    </Box>
+                )}
+                MenuProps={MenuProps}
+                >
+                {categories.map((pos) => (
+                    <MenuItem
+                    key={pos.name}
+                    value={pos.name}
+                    >
+                    {pos.name}
+                    </MenuItem>
+                ))}
+                </Select>
+            </FormControl>
+        )
+    }
+
     return (
         <div>
             <NavBar></NavBar>
+            <Paper style={card}>
+                <h3>Filter</h3>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                        <FilterCategories />
+                    </Grid>
+                </Grid>
+            </Paper>
             <Paper elevation={3} style={card}>
                 <h1>Petitions</h1>
                 <TableContainer component={Paper}>
